2c6d56c62e1e38de348d6de0f1df3b0e017d8815
nico-mania <55563178+nico-mania@users.noreply.github.com>
2023-02-07 20:29:50 +0100

    added new Scene for Quiz added color change für right and wrong answers

--- Assets/Scripts/Answers.cs
+++ Assets/Scripts/Answers.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class Answers : MonoBehaviour
@@ -7,17 +9,26 @@ public class Answers : MonoBehaviour
    public bool isCorrect = false;
    public QuizManager quizManager;
 
+   [SerializeField] public Color startColor;
+
+   private void Start()
+   {
+      startColor = GetComponent<RawImage>().color;
+   }
+
    public void Answer()
    {
       if (isCorrect)
       {
+         GetComponent<RawImage>().color = Color.green;
          Debug.Log("Correct");
-         quizManager.correctAnswer();
+         quizManager.CorrectAnswer();
       }
       else
       {
+         GetComponent<RawImage>().color = Color.red;
          Debug.Log("Wrong");
-         quizManager.correctAnswer();
+         quizManager.WrongAnswer();
       }
    }
 }
--- Assets/Scripts/QuizManager.cs
+++ Assets/Scripts/QuizManager.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class QuizManager : MonoBehaviour
 {
@@ -10,23 +13,67 @@ public class QuizManager : MonoBehaviour
     public GameObject[] options;
     public int currentQuestion;
 
-    public TMP_Text QuestionText;
+    [SerializeField] private GameObject QuizPanel;
+    [SerializeField] private GameObject RetryPanel;
+    [SerializeField] private Button RetryButton;
+    [SerializeField] private TMP_Text QuestionText;
+    [SerializeField] private TMP_Text quizScore;
+    
+    private int totalQuestions = 0;
+    private int rightAnswers = 0;
 
     private void Start()
     {
-        generateQuestion();
+        totalQuestions = QnA.Count;
+        NextQuestion();
+        RetryButton.onClick.AddListener(Retry);
+        //fixt momentan fehler indem die buttons nach scenen neustart nicht wieder weiß werden
+        for (int i = 0; i < options.Length; i++)
+        {
+            options[i].GetComponent<RawImage>().color =  Color.white;
+        }
     }
 
-    public void correctAnswer()
+    private void GameOver()
+    {
+        QuizPanel.SetActive(false);
+        RetryPanel.SetActive(true);
+        quizScore.text = rightAnswers + "/" + totalQuestions;
+    }
+
+    public void Retry()
+    {
+        Debug.Log("Retry");
+        QuizPanel.SetActive(true);
+        RetryPanel.SetActive(false);
+        SceneManager.UnloadSceneAsync("QuizScene");
+        SceneManager.LoadScene("QuizScene", LoadSceneMode.Additive);
+    }
+    
+    public void CorrectAnswer()
+    {
+        rightAnswers += 1;
+        QnA.RemoveAt(currentQuestion);
+        StartCoroutine(WaitForNextQuestion());
+    }
+
+    public void WrongAnswer()
     {
         QnA.RemoveAt(currentQuestion);
-        generateQuestion();
+        StartCoroutine(WaitForNextQuestion());
+    }
+
+    private IEnumerator WaitForNextQuestion()
+    {
+        yield return new WaitForSeconds(0.5f);
+        NextQuestion();
     }
 
     private void SetAnswers()
     {
         for (int i = 0; i < options.Length; i++)
         {
+            options[i].GetComponent <RawImage>().color = options[i].GetComponent <Answers>().startColor;
             options[i].GetComponent<Answers>().isCorrect = false;
             options[i].transform.GetChild(0).GetComponent<TMP_Text>().text = QnA[currentQuestion].Answers[i];
             
@@ -36,7 +83,7 @@ public class QuizManager : MonoBehaviour
             }
         }
     }
-    private void generateQuestion()
+    private void NextQuestion()
     {
         if (QnA.Count > 0)
         {
@@ -48,6 +95,7 @@ public class QuizManager : MonoBehaviour
         else
         {
             Debug.Log("Out of Questions.");
+            GameOver();
         }
     }
 }
